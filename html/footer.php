cb28d8840669b4dc82cdf412e018b6bea388e466
Xaverine POBELLE <xaverinepobelle@orange.fr>
2016-09-21 17:35:15 +0200

    code footer

---
+++ html/footer.php
@@ -0,0 +1,5 @@
+<div id="container-fluid">
+        <div class="col-md-8 col-md-offset-2 footer">
+            <h3>Created by Xaverine POBELLE & Axel GUILLAUME | <a href="#">Mentions Légales</a></h3>
+        </div>
+    </div>
