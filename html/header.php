b487a58e3670aba809abf793a93defbf814af363
Xaverine POBELLE <xaverinepobelle@orange.fr>
2016-09-22 14:39:57 +0200

    Reccup' fichier master

---
+++ html/header.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="glyphicon glyphicon-align-justify"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    Sneakers FAN
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Accueil</a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Actualités</a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Galerie</a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+    </nav>
