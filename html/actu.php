7417ff73656a376ba1d1a35ab9e0b997d6b0b32d
Xaverine POBELLE <xaverinepobelle@orange.fr>
2016-09-22 17:49:32 +0200

    ajout fichier actu.php

---
+++ html/actu.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sneakers FAN</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css" >
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" >
+	<link rel="stylesheet" href="../css/style.css">
+	<script type="text/javascript" src="../bootstrap/js/bootstrap.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <!-- SCROLL -->
+    <script type="text/javascript" src="../bootstrap/js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="../bootstrap/js/jquery.localscroll-1.2.7-min.js"></script>
+    <script type="text/javascript" src="../bootstrap/js/jquery.scrollTo-1.4.3.1-min.js"></script>
+    <script type="text/javascript">
+    jQuery(function($){
+        $.localScroll();
+         
+    });
+    </script>
+</head>
+
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+	<!--  NAV  -->
+<<<<<<< Updated upstream:html/index.php
+	
+<?php include("header.php"); ?>
+=======
+	<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse" href="#1">
+                    <i class="glyphicon glyphicon-align-justify"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    Sneakers FAN
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Accueil</a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Actualités</a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Galerie</a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+    </nav>
+
+
+    <!--  Accueil  -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">SNEAKERS FAN</h1>
+                        <a href="#bla" class="fleche">
+                            <i class="glyphicon glyphicon-chevron-down"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!--  Section 1  -->
+
+    <div id="container-fluid">
+        <div class="row section">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <h1>Qui sommes nous?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quas ratione eveniet rerum, dolorem beatae soluta dicta nam inventore, temporibus maxime molestiae deleniti, quo itaque quia provident autem. Laboriosam, itaque.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora odit iusto saepe praesentium exercitationem odio delectus. Totam eligendi harum incidunt, repudiandae doloribus odit placeat sunt, labore quis dolor at molestiae.</p>
+                    <p class="signature">By Xavp...</p>
+                </div>
+                <div class="col-md-6">
+                    <img class="image_article" src="../images_article/article_1.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--  Section 2  -->
+
+    <div id="container-fluid">
+        <div class="row section">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <h1>Qui sommes nous?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quas ratione eveniet rerum, dolorem beatae soluta dicta nam inventore, temporibus maxime molestiae deleniti, quo itaque quia provident autem. Laboriosam, itaque.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora odit iusto saepe praesentium exercitationem odio delectus. Totam eligendi harum incidunt, repudiandae doloribus odit placeat sunt, labore quis dolor at molestiae.</p>
+                    <p class="signature">By Xavp...</p>
+                </div>
+                <div class="col-md-6">
+                    <img class="image_article" src="../images_article/article_2.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--  Section 3  -->
+
+    <div id="container-fluid">
+        <div class="row section">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <h1>Qui sommes nous?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quas ratione eveniet rerum, dolorem beatae soluta dicta nam inventore, temporibus maxime molestiae deleniti, quo itaque quia provident autem. Laboriosam, itaque.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora odit iusto saepe praesentium exercitationem odio delectus. Totam eligendi harum incidunt, repudiandae doloribus odit placeat sunt, labore quis dolor at molestiae.</p>
+                    <p class="signature">By Xavp...</p>
+                </div>
+                <div class="col-md-6">
+                    <img class="image_article" src="../images_article/article_3.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--  Section 4  -->
+
+    <div id="container-fluid">
+        <div class="row section">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <h1>Qui sommes nous?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quas ratione eveniet rerum, dolorem beatae soluta dicta nam inventore, temporibus maxime molestiae deleniti, quo itaque quia provident autem. Laboriosam, itaque.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora odit iusto saepe praesentium exercitationem odio delectus. Totam eligendi harum incidunt, repudiandae doloribus odit placeat sunt, labore quis dolor at molestiae.</p>
+                    <p class="signature">By Xavp...</p>
+                </div>
+                <div class="col-md-6">
+                    <img class="image_article" src="../images_article/article_4.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--  Section 5  -->
+
+    <div id="container-fluid">
+        <div class="row section">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <h1>Qui sommes nous?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quas ratione eveniet rerum, dolorem beatae soluta dicta nam inventore, temporibus maxime molestiae deleniti, quo itaque quia provident autem. Laboriosam, itaque.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora odit iusto saepe praesentium exercitationem odio delectus. Totam eligendi harum incidunt, repudiandae doloribus odit placeat sunt, labore quis dolor at molestiae.</p>
+                    <p class="signature">By Xavp...</p>
+                </div>
+                <div class="col-md-6">
+                    <img class="image_article" src="../images_article/article_5.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--  FOOTER  -->
+
+    <?php include("footer.php"); ?>
+
+
+</body>
+</html>
