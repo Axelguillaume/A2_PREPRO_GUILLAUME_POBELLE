5a0aa79e2fb62b977aaff310ac7eac7d7ccb185f
Unknown <guilllaumeaxel@gmail.com>
2016-09-22 18:37:08 +0200

    Mise en place de la page Contact

---
+++ html/contact.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Sneakers FAN</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css" >
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" >
+	<link rel="stylesheet" href="../css/style.css">
+	<script type="text/javascript" src="../bootstrap/js/bootstrap.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+	</head>
+
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+	<!--  NAV  -->
+	
+<?php include("header.php"); ?>
+
+    <!--  Accueil  -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">CONTACT</h1>
+                        <a href="#bla" class="fleche">
+                            <i class="glyphicon glyphicon-chevron-down"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!--  TITRE FORMULAIRE  -->
+
+    <div id="container-fluid">
+    	<div class="row">
+	    	<div class="col-md-12 txt">
+	    		<h1>Des questions des conseils? N'hésitez pas !</h1>
+			</div>
+		</div>
+    </div>
+    		
+
+    <!--  FOOTER  -->
+
+    <?php include("footer.php"); ?>
+
+</body>
+</html>
