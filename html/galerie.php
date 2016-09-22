a33e26e4745adb37bb90ae4247ed4d661ce82c43
Xaverine POBELLE <xaverinepobelle@orange.fr>
2016-09-22 14:44:31 +0200

    ajout html galerie.php

--- html/galerie.php
+++ html/galerie.php
@@ -1,16 +1,87 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta charset="UTF-8">
+  <title>Sneakers FAN</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css" >
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" >
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>S Gallery: Responsive jQuery Gallery Plugin with CSS3 Animations</title>
-
-  
+  <link rel="stylesheet" href="../css/style.css">
   <link rel="stylesheet" href="../css/styles_galerie.css" />
-
+  <script type="text/javascript" src="../bootstrap/js/bootstrap.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <!-- SCROLL -->
+    <script type="text/javascript" src="../bootstrap/js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="../bootstrap/js/jquery.localscroll-1.2.7-min.js"></script>
+    <script type="text/javascript" src="../bootstrap/js/jquery.scrollTo-1.4.3.1-min.js"></script>
+    <script type="text/javascript">
+    jQuery(function($){
+        $.localScroll();
+         
+    });
+    </script>
 </head>
 
-<body>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+  <!--  NAV  -->
+
+  
+<?php include("header.php"); ?>
+
+  <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse" href="#1">
+                    <i class="glyphicon glyphicon-align-justify"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    Sneakers FAN
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Accueil</a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Actualités</a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Galerie</a>
+                    </li>
+                    <li>
+                        <a class="" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+    </nav>
+
+    <!--  Accueil  -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">GALERIE PHOTOS</h1>
+                        <a href="#bla" class="fleche">
+                            <i class="glyphicon glyphicon-chevron-down"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
 <div class="demo-wrapper">
 
@@ -153,6 +224,11 @@
       });
     });
   </script>
+
+    <!--  FOOTER  -->
+
+    <?php include("footer.php"); ?>
+
 
 </body>
 </html>
