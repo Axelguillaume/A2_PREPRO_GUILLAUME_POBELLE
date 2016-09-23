ea546e9ba2e91d1772370ce953a85d6eb30a9793
Xaverine POBELLE <xaverinepobelle@orange.fr>
2016-09-23 14:36:47 +0200

    supp fichiers inutiles

--- html/footer.php
+++
@@ -1,5 +0,0 @@
-<div id="container-fluid">
-        <div class="col-md-8 col-md-offset-2 footer">
-            <h3>Created by Xaverine POBELLE & Axel GUILLAUME | <a href="#">Mentions Légales</a></h3>
-        </div>
-    </div>
