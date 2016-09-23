7143b67a36774948ff371284b2d96b7bd89b6231
Unknown <guilllaumeaxel@gmail.com>
2016-09-23 14:25:19 +0200

    Fix lien header

--- html/actu.php
+++ html/actu.php
@@ -30,41 +30,6 @@
 	
 <?php include("header.php"); ?>
 
-	<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse" href="#1">
-                    <i class="glyphicon glyphicon-align-justify"></i>
-                </button>
-                <a class="navbar-brand page-scroll" href="#page-top">
-                    Sneakers FAN
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="" href="#">Accueil</a>
-                    </li>
-                    <li>
-                        <a class="" href="#">Actualités</a>
-                    </li>
-                    <li>
-                        <a class="" href="#">Galerie</a>
-                    </li>
-                    <li>
-                        <a class="" href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            
-        </div>
-    </nav>
-
-
     <!--  Accueil  -->
     <header class="intro_actu">
         <div class="intro-body">
